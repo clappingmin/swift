fba303f7ec266003ed83f577b7ef715e99b7fe40
clappingmin <clappingmin@naver.com>
2021-09-16 22:59:57 +0900

    Flow Control

--- 스위프트_Flow_Control.playground/Contents.swift
+++ 스위프트_Flow_Control.playground/Contents.swift
@@ -49,3 +49,95 @@ var sinValue: CGFloat = 0
 for i in closedRange {
     sinValue = sin(CGFloat.pi/4 * CGFloat(i))
 }
+
+let name = "Jason"
+for _ in closedRange {
+    print("---> name : \(name)")
+}
+
+for i in closedRange {
+    
+    if i % 2 == 0 {
+        continue
+        
+    }
+    print("---> \(i)")
+}
+
+for i in closedRange where i % 2 == 0 {
+    print("--->짝수 :  \(i)")
+}
+
+for i in closedRange {
+    
+    if i == 3 {
+        continue
+        
+    }
+    print("---> \(i)")
+}
+
+for i in closedRange {
+    for j in closedRange {
+        print("gugudan --> \(i) * \(j) = \(i*j)")
+    }
+}
+
+
+// --- Switch
+
+let num = 10
+
+switch num {
+case 0:
+    print("---> 0")
+case 0...10:
+    print("---> 0~10 사이 입니다.")
+case 10:
+    print("---> 10")
+default:
+    print("---> 나머지 입니다.")
+}
+
+let pet = "bird"
+
+switch pet {
+case "dog", "cat", "bird":
+    print("집 동물이네여?")
+default:
+    print("잘 모르겠습니다.")
+}
+
+let number = 5
+switch number {
+case _ where number % 2 == 0 :
+    print("---> 짝수")
+default:
+    print("---> 홀수")
+}
+
+let cordinate = (x: 10, y:10)
+
+switch cordinate {
+case (0,0):
+    print("원점입니다.")
+case (_,0):
+    print("x축")
+case (0,_):
+    print("y축")
+default:
+    print("좌표 어딘가")
+}
+
+switch cordinate {
+case (0,0):
+    print("원점입니다.")
+case (let x,0):
+    print("x축, x:\(x)")
+case (0,let y):
+    print("y축, y:\(y)")
+case (let x, let y) where x == y:
+    print("좌표 어딘가, x랑 y랑 같음, x,y = \(x),\(y)")
+case (let x, let y):
+    print("좌표 어딘가, x,y = \(x),\(y)")
+}
