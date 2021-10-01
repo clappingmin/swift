f9e38bcd130c07c508dc756c0cba5ce6f0a43665
clappingmin <clappingmin@naver.com>
2021-10-02 01:17:06 +0900

    Function and Optional

--- 스위프트_Optional.playground/Contents.swift
+++ 스위프트_Optional.playground/Contents.swift
@@ -45,3 +45,44 @@ func printParsedInt(from: String){
 
 printParsedInt(from: "100")
 printParsedInt(from: "헬로우")
+
+// 3.
+func printParsedInt2(from: String){
+    guard let parsedInt = Int(from) else{
+        print("Int로 컨버팅 안된다.")
+        return
+    }
+    print(parsedInt)
+}
+
+printParsedInt2(from: "100")
+printParsedInt2(from: "헬로우")
+
+// 4.
+
+carName = nil
+let myCarName: String = carName ?? "model S"
+
+
+// --- 도전 과제
+// 1. 최애 음식이름을 담는 변수를 작성(String?),
+// 2. optional binding 이용해서 값을 확이해보기
+// 3. 닉네임을 받아서 출력하는 함수 만들기, 조건 입력 파라미터는 String?
+
+let favoritFood: String? = "양고기"
+
+if let foodName = favoritFood{
+    print(foodName)
+}else {
+    print("non food")
+}
+
+func printNickName(name: String?){
+    guard let nickName = name else{
+        print("non name")
+        return
+    }
+    print(nickName)
+}
+
+printNickName(name : nil)
