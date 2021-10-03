463aef9c3c06a053ad9790bb8a56174d83b029fa
clappingmin <clappingmin@naver.com>
2021-10-04 00:09:48 +0900

    Array

--- 스위프트_Array.playground/Contents.swift
+++ 스위프트_Array.playground/Contents.swift
@@ -43,5 +43,35 @@ evenNumbers.insert(1, at: 0) // 0번째에 1을 넣겠다.
 evenNumbers.remove(at: 0)
 evenNumbers
 
-evenNumbers.removeAll()
-evenNumbers = []
+evenNumbers[0] = -2
+evenNumbers[0...2] = [-2, 0, 2]
+evenNumbers
+
+//evenNumbers.removeAll()
+//evenNumbers = []
+
+evenNumbers.swapAt(0, 1) // 0번째와 1번째를 바꾸기
+
+for num in evenNumbers{
+    print(num)
+}
+
+for (index, num) in evenNumbers.enumerated(){
+    print("idx : \(index), value : \(num)")
+}
+
+evenNumbers.dropFirst(3) // 앞에 3개 없앤 결과를 보기, array에 실제로 영향을 주지 않는다.
+evenNumbers
+
+let firstThreeRemoved = evenNumbers.dropFirst(3)
+firstThreeRemoved
+
+let lastDroped = evenNumbers.dropLast(1)
+lastDroped
+
+let firstThree1 = evenNumbers.prefix(3) // 앞에서 3개를 가져옴
+firstThree1
+
+let lastThree = evenNumbers.suffix(3)
+lastThree
+
